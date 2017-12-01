7284b59d5faeef4561a91f708f8f1e3b575a705a
Dmitry Novotochinov <trybeee@gmail.com>
2017-12-01 12:56:39 +0300

    update lein profiles

--- profiles.clj
+++ profiles.clj
@@ -1,11 +1,13 @@
 {:user  {:dependencies [[alembic "0.3.2"]
                         [org.clojure/tools.nrepl "0.2.12"] ]
-         :plugins  [[lein-ancient "0.6.10"]
+         :plugins  [[jonase/eastwood "0.2.3"]
+                    [lein-ancient "0.6.10"]
                     [lein-cljfmt "0.5.6"]
                     [lein-kibit "0.1.2"]
                     [lein-hiera "0.9.5"]
+                    [lein-re-frisk "0.5.3"]
                     [lein-vanity "0.2.0"]
-                    [jonase/eastwood "0.2.3"]
+                    [lein-localrepo "0.5.4"]
                     [venantius/yagni "0.1.4"]
                     [venantius/ultra "0.5.0"]]
          :aliases {"anc" ^{:doc "Check all dependencies for updates"}
@@ -19,6 +21,6 @@
                    "dev" ^{:doc "Run development repl"}
                    ["do" "clean" ["repl" ":headless"]]}
          :ultra {:color-scheme :solarized_dark}}
- :repl {:plugins [[cider/cider-nrepl "0.15.0-SNAPSHOT"]
+ :repl {:plugins [[cider/cider-nrepl "0.15.1-SNAPSHOT"]
                   [refactor-nrepl "2.3.0-SNAPSHOT"]]}}
 
